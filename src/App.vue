df63b18ff61d96bc905cf58c5ac64a08a1683971
gabriela <gubriana@gmail.com>
2020-07-13 12:34:43 -0400

    New: listas de dos componentes distintos

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Estas son dos listas:"/>
+    <ListaTech/>
+    <ListaFinancial/>
+  </div>
+</template>
+
+<script>
+
+import ListaTech from './components/ListaTech.vue'
+import ListaFinancial from './components/ListaFinancial.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ListaTech,
+    ListaFinancial
+  }
+}
+
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListaTech.vue
@@ -0,0 +1,39 @@
+<template>
+        <div id = "app">
+            <h1 v-if = "tech">Lista de {{ nombre }} de tecnologías que quiere aprender:</h1>
+            <ul>
+                <li v-for = "(techs, i) in tech" v-bind:key="i" >{{techs}}</li>
+            </ul>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'ListaTech',
+  data:function() {
+    return{
+      nombre: "Gabriela",
+      tech: true,
+      tech: ['HTML', 'CSS', 'Vue', 'Firebase',] 
+    }
+  }  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
