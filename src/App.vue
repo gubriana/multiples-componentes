607437de8f064c91da5e53eb5c61dda34420629d
gabriela <gubriana@gmail.com>
2020-07-14 09:43:34 -0400

    add: funcionando

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Estas son dos listas:"/>
-    <ListaTech/>
-    <ListaFinancial/>
+    <a href="https://www.freepik.es/fotos-vectores-gratis/viajes" target="_blank"><img alt="Vue logo" src="./assets/logo.png"></a>
+    <GListas titulo="Paises a los que quiero viajar" v-bind:enlaces="Paises_Si"/>
+    <GListas titulo="Paises a los que no quiero viajar" v-bind:enlaces="Paises_No"/>
   </div>
 </template>
-
 <script>
-
-import ListaTech from './components/ListaTech.vue'
-import ListaFinancial from './components/ListaFinancial.vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    ListaTech,
-    ListaFinancial
-  }
-}
-
+  import GListas from './components/GListas.vue'
+  export default {
+    name: 'App',
+    components: {
+      GListas
+    },
+    data: () => {
+      return {
+        Paises_Si:[
+          {texto:"Japón",url:"http://japon.jp/"},
+          {texto:"Islandia",url:"http://www.islandia.is"},
+          {texto:"Noruega",url:"https://en.wikipedia.org/wiki/Norway"},
+          {texto:"Suecia",url:"http://www.sweden.se"},
+          {texto:"Turquia",url:"https://en.wikipedia.org/wiki/Turquia"},
+          {texto:"India",url:"https://en.wikipedia.org/wiki/India"}
+        ],
+        Paises_No:[
+          {texto:"Estados Unidos",url:"https://en.wikipedia.org/wiki/Usa"},
+          {texto:"Perú",url:"https://en.wikipedia.org/wiki/Peru"},
+          {texto:"China",url:"https://en.wikipedia.org/wiki/China"},
+          {texto:"Francia",url:"http://www.france.fr"}
+        ]
+      };
+    }
+  };
 </script>
 <style>
 #app {
@@ -33,3 +42,15 @@ export default {
   margin-top: 60px;
 }
 </style>
+© 2020 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
